30978c41f3a6f88eb255f02bc7c3d790165f0330
Miguel Angel López <admon@tecnott.com.mx>
2022-09-30 16:59:18 -0500

    Changes the serializer UserSerializer for display specific attributes in the list

--- ecommerce/ecommerce_rest/apps/users/api/serializers.py
+++ ecommerce/ecommerce_rest/apps/users/api/serializers.py
@@ -9,34 +9,12 @@ class UserSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class TestUserSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=200)
-    email = serializers.EmailField()
-
-    def validate_name(self, value):
-        if "developer" in value:
-            raise serializers.ValidationError(
-                "Error, no puede existir un usuario con ese nombre"
-            )
-        return value
-
-    def validate_email(self, value):
-        if value == "":
-            raise serializers.ValidationError("Tiene que indicar un correo")
-
-        return value
-
-    def validate(self, data):
-        return data
-
-    def create(self, validated_data):
-        print(validated_data)
-        return User.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.email = validated_data.get('email', instance.email)
-        instance.save()
-        return instance
+    def to_representation(self, instance):
         
+        return {
+            'id': instance['id'],
+            'nombre_usuario': instance['username'],
+            'correo_electronico': instance['email'],
+            'clave': instance['password']
+        }
 
