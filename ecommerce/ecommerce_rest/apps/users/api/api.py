e7bdf607912d907a1bba1f9651caa8b56f8f36db
Miguel Angel López <admon@tecnott.com.mx>
2022-09-29 19:32:57 -0500

    Examples of serializers with APIView and function with decorator @api_view

---
+++ ecommerce/ecommerce_rest/apps/users/api/api.py
@@ -0,0 +1,25 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from apps.users.models import User
+from apps.users.api.serializers import UserSerializer
+from rest_framework.decorators import api_view
+
+class UserAPIView(APIView):
+
+    def get(self,request):
+        users = User.objects.all()
+        users_serializer = UserSerializer(users, many=True)
+        return Response(users_serializer.data)
+
+
+        
+@api_view(['GET'])
+def user_api_view(request):
+
+    if request.method == 'GET':
+        users = User.objects.all()
+        users_serializer = UserSerializer(users, many=True)
+        return Response(users_serializer.data)
+
+
+
