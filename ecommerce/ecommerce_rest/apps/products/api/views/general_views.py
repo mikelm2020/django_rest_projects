32eb4373224a4822976ea6dcf880a9cb153f878a
Miguel Angel López <admon@tecnott.com.mx>
2022-10-13 17:55:11 -0500

    Added Operations update and destroy in CRUD for catagorys in Project Ecommerce

--- ecommerce/ecommerce_rest/apps/products/api/views/general_views.py
+++ ecommerce/ecommerce_rest/apps/products/api/views/general_views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework import status
 
 from apps.products.api.serializers.general_serializers import (
     MeasureUnitSerializer,
@@ -38,7 +39,53 @@ class CategoryProductViewSet(viewsets.GenericViewSet):
     def get_queryset(self):
         return self.get_serializer().Meta.model.objects.filter(state=True)
 
+    def get_object(self):
+        return self.get_serializer().Meta.model.objects.filter(
+            id=self.kwargs["pk"], state=True
+        )
+
     def list(self, request):
         data = self.get_queryset()
         data = self.get_serializer(data, many=True)
         return Response(data.data)
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "Categoría registrada correctamente!"},
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(
+            {"message": "", "error": "Categoría no encontrada!"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    def update(self, request, pk=None):
+        if self.get_object().exists():
+            serializer = self.serializer_class(
+                instance=self.get_object().get(), data=request.data
+            )
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {"message": "Categoría atualizada correctamente!"},
+                    status=status.HTTP_200_OK,
+                )
+        return Response(
+            {"message": "", "error": serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    def destroy(self, request, pk=None):
+        if self.get_object().exists():
+            self.get_object().get().delete()
+            return Response(
+                {"message": "Categoría eliminada correctamente!"},
+                status=status.HTTP_200_OK,
+            )
+        return Response(
+            {"message": "", "error": "Categoría no encontrda!"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
