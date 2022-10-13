26ef801b917a3daf07ed024ee4e94c559db13f7a
Miguel Angel López <admon@tecnott.com.mx>
2022-10-13 01:34:25 -0500

    Added CORS configuration in Django settings

--- ecommerce/ecommerce_rest/ecommerce_rest/settings/common.py
+++ ecommerce/ecommerce_rest/ecommerce_rest/settings/common.py
@@ -49,6 +49,7 @@ LOCAL_APPS = [
 ]
 
 THIRD_APPS = [
+    "corsheaders",
     "rest_framework",
     "rest_framework.authtoken",
     "simple_history",
@@ -57,13 +58,12 @@ THIRD_APPS = [
 
 INSTALLED_APPS = BASE_APPS + LOCAL_APPS + THIRD_APPS
 
-SWAGGER_SETTINGS = {
-    'DOC_EXPANSION': 'none'
-}
+SWAGGER_SETTINGS = {"DOC_EXPANSION": "none"}
 
 TOKEN_EXPIRED_AFTER_SECONDS = 900
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -126,6 +126,15 @@ USE_I18N = True
 USE_TZ = True
 
 AUTH_USER_MODEL = "users.User"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+]
+
 
 
 # Static files (CSS, JavaScript, Images)
