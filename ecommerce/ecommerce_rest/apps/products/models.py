de791a5eb0fa503f39b06f01450464e75958ec78
Miguel Angel López <admon@tecnott.com.mx>
2022-10-22 01:55:52 -0500

    Added stock field how property in Products Model for the app Products in ecommerce_rest project

--- ecommerce/ecommerce_rest/apps/products/models.py
+++ ecommerce/ecommerce_rest/apps/products/models.py
@@ -96,3 +96,14 @@ class Product(BaseModel):
     def __str__(self):
         """Unicode representation of Product."""
         return self.name
+
+    @property
+    def stock(self):
+        from django.db.models import Sum
+        from apps.expense_manager.models import Expense
+
+        expenses = Expense.objects.filter(product=self, state=True).aggregate(
+            Sum("quantity")
+        )
+
+        return expenses
